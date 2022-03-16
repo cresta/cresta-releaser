79f8cd27ec84f55ba8b746465d952e9003e4511d
Jack Lindamood <jack@cresta.ai>
2022-03-16 09:33:28 -0700

    comments

--- mage/releaser.go
+++ mage/releaser.go
@@ -52,6 +52,7 @@ func getOutputFormat() commands.OutputFormatter {
 	}
 }
 
+// ListReleases will list all releases for an application
 func ListReleases(_ context.Context, application string) error {
 	releases, err := MustGetInstance().ListReleases(application)
 	if err != nil {
@@ -60,6 +61,7 @@ func ListReleases(_ context.Context, application string) error {
 	return getOutputFormat().WriteStringSlice(os.Stdout, releases)
 }
 
+// ListApplications will list all applications
 func ListApplications(_ context.Context) error {
 	apps, err := MustGetInstance().ListApplications()
 	if err != nil {
@@ -68,6 +70,7 @@ func ListApplications(_ context.Context) error {
 	return getOutputFormat().WriteStringSlice(os.Stdout, apps)
 }
 
+// GetRelease will get a release for an application
 func GetRelease(_ context.Context, application string, release string) error {
 	out, err := MustGetInstance().GetRelease(application, release)
 	if err != nil {
@@ -76,6 +79,8 @@ func GetRelease(_ context.Context, application string, release string) error {
 	return getOutputFormat().WriteObject(os.Stdout, out)
 }
 
+// PreviewRelease will show what a new release will look like, promoting from the previous version.  It returns the
+// old release and the new release.
 func PreviewRelease(_ context.Context, application string, release string) error {
 	oldRelease, newRelease, err := MustGetInstance().PreviewRelease(application, release)
 	if err != nil {
@@ -87,6 +92,8 @@ func PreviewRelease(_ context.Context, application string, release string) error
 	return getOutputFormat().WriteString(os.Stdout, d.DiffPrettyText(diffs))
 }
 
+// ApplyRelease will promote a release to be the current version by applying the previously
+// fetched PreviewRelease
 func ApplyRelease(_ context.Context, application string, release string) error {
 	oldRelease, newRelease, err := MustGetInstance().PreviewRelease(application, release)
 	if err != nil {
@@ -95,23 +102,31 @@ func ApplyRelease(_ context.Context, application string, release string) error {
 	return MustGetInstance().ApplyRelease(application, release, oldRelease, newRelease)
 }
 
+// FreshGitBranch will create a fresh git branch for releasing.  The name of the branch will somewhat match the
+// release + application name.
 func FreshGitBranch(ctx context.Context, application string, release string) error {
 	forcedName := os.Getenv("FORCED_NAME")
 	return MustGetInstance().FreshGitBranch(ctx, application, release, forcedName)
 }
 
+// CommitForRelease will commit the release to the git branch.  It assumes you've already called ApplyRelease
 func CommitForRelease(ctx context.Context, application string, release string) error {
 	return MustGetInstance().CommitForRelease(ctx, application, release)
 }
 
+// ForcePushCurrentBranch will force push the current branch to the remote repository as a branch with the same name.
+// Fails on branches master or main.
 func ForcePushCurrentBranch(ctx context.Context) error {
 	return MustGetInstance().ForcePushCurrentBranch(ctx)
 }
 
+// PullRequestCurrent creates a pull request for the current branch
 func PullRequestCurrent(ctx context.Context) error {
 	return MustGetInstance().PullRequestCurrent(ctx)
 }
 
+// CheckForPROnCurrentBranch will check if there is a pull request on the current branch.  Returns 0 if there is no
+// PR, otherwise the PR number
 func CheckForPROnCurrentBranch(ctx context.Context) error {
 	i, err := MustGetInstance().CheckForPROnCurrentBranch(ctx)
 	if err != nil {
@@ -121,6 +136,7 @@ func CheckForPROnCurrentBranch(ctx context.Context) error {
 	return nil
 }
 
+// GithubWhoami returns who the CLI thinks you are on github
 func GithubWhoami(ctx context.Context) error {
 	s, err := MustGetInstance().GithubWhoami(ctx)
 	if err != nil {
@@ -129,10 +145,12 @@ func GithubWhoami(ctx context.Context) error {
 	return getOutputFormat().WriteString(os.Stdout, s)
 }
 
+// ApprovePullRequestForCurrentRemote will approve the pull request on the current remote
 func ApprovePullRequestForCurrentRemote(ctx context.Context, approvalMessage string, prNumber int64) error {
 	return MustGetInstance().ApprovePullRequestForCurrentRemote(ctx, approvalMessage, prNumber)
 }
 
+// MergePullRequestForCurrentRemote will merge an approved PR
 func MergePullRequestForCurrentRemote(ctx context.Context, prNumber int64) error {
 	return MustGetInstance().MergePullRequestForCurrentRemote(ctx, prNumber)
 }
